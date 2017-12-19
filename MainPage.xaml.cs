5165273ce28b9bff3bf2b058c5d0757e413db05c
NiallMcCann10 <34485940+NiallMcCann10@users.noreply.github.com>
2017-12-19 10:56:29 +0000

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace Timer
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        Stopwatch Timmer = new Stopwatch();
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Timmer.Start();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text = Timmer.Elapsed.ToString();
+            string milliseconds = Timmer.ElapsedMilliseconds.ToString();
+            Timmer.Stop();
+        }
+
+    }
+}
